58e1a6bcc9fae9e6d988c1439cd970562786bd22
Imasha Senadheera <imashasenadheera326@gmail.com>
2023-12-05 12:30:18 +0530

    5th Commit

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,8 +1,22 @@
+// get data 
+db.collection('guides').get().then(snapshot => {
+  setupGuides(snapshot.docs);
+});
+
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+  if (user) {
+    console.log('User logged in: ', user);
+  } else {
+    console.log('User logged out!');
+  }
+});
+
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  
+
   // get user info
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
@@ -15,4 +29,31 @@ signupForm.addEventListener('submit', (e) => {
     M.Modal.getInstance(modal).close();
     signupForm.reset();
   });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut()
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+
 });
--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,31 @@
-// setup materialize components
-document.addEventListener('DOMContentLoaded', function() {
+// DOM elements
+const guideList = document.querySelector('.guides');
 
-    var modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals);
-  
-    var items = document.querySelectorAll('.collapsible');
-    M.Collapsible.init(items);
-  
+// setup guides
+const setupGuides = (data) => {
+
+  let html = '';
+  data.forEach(doc => {
+    const guide = doc.data();
+    const li = `
+      <li>
+      <div class="collapsible-header grey lighten-4" style="color: #8f7193; font-weight: bold;"> ${guide.title} </div>
+      <div class="collapsible-body white" style="color: #c0a0c3;"> ${guide.content} </div>
+      </li>
+    `;
+    html += li;
   });
+  guideList.innerHTML = html
+
+};
+
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function () {
+
+  var modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+
+  var items = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(items);
+
+});
